4e85bb5803bb1a7c29bdfdddc9384d61fbbf2907
yuri daniel <yuridanielsantana45@gmail.com>
2025-10-20 14:07:39 -0300

    inserindo produtos

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -9,7 +9,7 @@ def criar_tabela():
             id INT AUTO_INCREMENT PRIMARY KEY,
             nome VARCHAR(100)NOT NULL,
             categoria VARCHAR(50),
-            preco DECIMAL(10,2),
+            preço DECIMAL(10,2),
             quantidade INT
              )
             """)
@@ -19,6 +19,25 @@ def criar_tabela():
         finally:
             cursor.close()
             conexao.close()
+
+criar_tabela()
+
+def inserir_produtos(nome,categoria,preço,quantidade):
+    conexao,cursor = coneatar()
+    if conexao:
+        try:
+            cursor.execute(
+            "INSERT INTO produtos(nome,categoria,preço,quantidade) VALUES (%s,%s,%s,%s)",
+            (nome,categoria,preço,quantidade)
+            )
+            conexao.connit()
+        except Exception as erro:
+            print(f"erro ao inserir produtos")
+        finally:
+            cursor.close()
+            conexao.close()
+        
+                 
 
             
 
